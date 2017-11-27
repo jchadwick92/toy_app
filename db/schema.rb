e35649287c8824ddbdb9a77517f7b0f782795088
Joe Chadwick <jchadwick92@hotmail.co.uk>
2017-11-27 20:26:15 +0000

    Added state to our tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127155138) do
+ActiveRecord::Schema.define(version: 20171127171234) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "controller"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "state", default: "to_do"
   end
 
   create_table "users", force: :cascade do |t|
